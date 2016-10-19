32a39166d5b5ef7f6d524a5cc5990c8d64683857
stevensotelo <stevenbetancurt@hotmail.com>
2016-10-18 20:53:26 -0500

    init project

---
+++ MedicinaPrepagada/Controllers/EPSController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MedicinaPrepagada.Models.DBMedicinaPrepagada.Source;
+using MedicinaPrepagada.Models.DBMedicinaPrepagada.Repository;
+
+namespace MedicinaPrepagada.Controllers
+{
+    public class EPSController : Controller
+    {
+        private RepositoryEPS db = new RepositoryEPS();
+
+        // GET: EPS
+        public ActionResult Index()
+        {
+            return View(db.listar());
+        }
+
+        // GET: EPS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EPS ePS = db.buscarPorID(id.Value);
+            if (ePS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ePS);
+        }
+
+        // GET: EPS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EPS/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nombre,nit,telefono,nombre_contacto,apellidos_contacto,transferencia,numero_cuenta")] EPS ePS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.agregar(ePS);
+                db.guardar();
+                return RedirectToAction("Index");
+            }
+
+            return View(ePS);
+        }
+
+        // GET: EPS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EPS ePS = db.buscarPorID(id.Value);
+            if (ePS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ePS);
+        }
+
+        // POST: EPS/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_eps,nombre,nit,telefono,nombre_contacto,apellidos_contacto,transferencia,numero_cuenta")] EPS ePS)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(ePS).State = EntityState.Modified;
+                //db.SaveChanges();
+                db.editar(ePS);
+                db.guardar();
+                return RedirectToAction("Index");
+            }
+            return View(ePS);
+        }
+
+        // GET: EPS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EPS ePS = db.buscarPorID(id.Value);
+            if (ePS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ePS);
+        }
+
+        // POST: EPS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EPS ePS = db.buscarPorID(id);
+            db.eliminar(ePS);
+            db.guardar();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MedicinaPrepagada/Models/DBMedicinaPrepagada/Repository/RepositoryEPS.cs
@@ -0,0 +1,78 @@
+﻿using MedicinaPrepagada.Models.DBMedicinaPrepagada.Source;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MedicinaPrepagada.Models.DBMedicinaPrepagada.Repository
+{
+    public class RepositoryEPS
+    {
+        private EntitiesMedicinaPrepagada db { get; set; }
+
+        /// <summary>
+        /// Metodo Constructor
+        /// </summary>
+        public RepositoryEPS()
+        {
+            db = new EntitiesMedicinaPrepagada();
+        }
+
+        /// <summary>
+        /// Metodo que agregar una EPS a la base de datos
+        /// </summary>
+        /// <param name="entidad">Entidad a agregar</param>
+        public void agregar(EPS entidad)
+        {
+            db.EPS.Add(entidad);
+        }
+
+        /// <summary>
+        /// Metodo que elimina una EPS de la base de datos
+        /// </summary>
+        /// <param name="entidad">Entidad a eliminar</param>
+        public void eliminar(EPS entidad)
+        {
+            db.EPS.Attach(entidad);
+            db.EPS.Remove(entidad);
+        }
+
+        /// <summary>
+        /// Metodo que edita una EPS en la base de datos
+        /// </summary>
+        /// <param name="entidad">Entidad a editar</param>
+        public void editar(EPS entidad)
+        {
+            db.Entry(entidad).State = EntityState.Modified;
+        }
+
+        /// <summary>
+        /// Metodo que guarda los cambios en la base de datos
+        /// </summary>
+        public void guardar()
+        {
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Metodo que retorna todas las EPS de la base de datos
+        /// </summary>
+        /// <returns>Lista de EPS</returns>
+        public IQueryable<EPS> listar()
+        {
+            return db.EPS;
+        }
+
+        /// <summary>
+        /// Metodo que busca una EPS por su ID
+        /// </summary>
+        /// <param name="id">Id de la EPS</param>
+        /// <returns></returns>
+        public EPS buscarPorID(int id)
+        {
+            return db.EPS.SingleOrDefault(p => p.id_eps == id);
+        }
+        
+    }
+}
