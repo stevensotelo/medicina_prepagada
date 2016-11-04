7e8f1f9d355dff35b6455225738ef5af45c778f8
stevensotelo <stevenbetancurt@hotmail.com>
2016-11-03 19:49:05 -0500

    cambio de fuente de datos

---
+++ MedicinaPrepagada/Controllers/IPSController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MedicinaPrepagada.Models.DBMedicinaPrepagada.Source;
+
+namespace MedicinaPrepagada.Controllers
+{
+    public class IPSController : Controller
+    {
+        private DBPrepagadaEntities db = new DBPrepagadaEntities();
+
+        // GET: IPS
+        public ActionResult Index()
+        {
+            return View(db.IPS.ToList());
+        }
+
+        // GET: IPS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IPS iPS = db.IPS.Find(id);
+            if (iPS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iPS);
+        }
+
+        // GET: IPS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: IPS/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_ips,nombre,nit,telefono,nombre_contacto,apellidos_contacto,transferencia,numero_cuenta")] IPS iPS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.IPS.Add(iPS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(iPS);
+        }
+
+        // GET: IPS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IPS iPS = db.IPS.Find(id);            
+            if (iPS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iPS);
+        }
+
+        // POST: IPS/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_ips,nombre,nit,telefono,nombre_contacto,apellidos_contacto,transferencia,numero_cuenta")] IPS iPS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iPS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(iPS);
+        }
+
+        // GET: IPS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IPS iPS = db.IPS.Find(id);
+            if (iPS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iPS);
+        }
+
+        // POST: IPS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            IPS iPS = db.IPS.Find(id);
+            db.IPS.Remove(iPS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
